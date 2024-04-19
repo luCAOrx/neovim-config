87238faf90d1bc41e4ced412152f3d529a40f474
luCAOrx <lucaorx@protonmail.com>
2024-04-19 20:09:08 -0300

    chore: Change icons and add new settings

--- plugin/lua/plugins/nvimtree.lua
+++ plugin/lua/plugins/nvimtree.lua
@@ -17,14 +17,32 @@ require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
   },
+  git = {
+    enable = true,
+    ignore = true,
+  },
+  filesystem_watchers = {
+    enable = true,
+  },
+  actions = {
+    open_file = {
+      resize_window = true,
+    },
+  },
   view = {
+    adaptive_size = false,
+    side = "left",
     width = 30,
+    preserve_window_proportions = true,
   },
   renderer = {
     group_empty = true,
     full_name = true,
     special_files = {},
     symlink_destination = false,
+    root_folder_label = true,
+    highlight_git = true,
+    highlight_opened_files = "none",
     indent_markers = {
       enable = true,
     },
@@ -36,10 +54,33 @@ require("nvim-tree").setup({
         folder_arrow = true,
         git = true,
       },
+      glyphs = {
+        default = "󰈚",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      }
     },
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
     custom = {
       "^.git$",
     },
@@ -47,18 +88,24 @@ require("nvim-tree").setup({
   diagnostics = {
     enable = true,
     show_on_dirs = true,
+    icons = {
+      hint = "󰌶",
+      info = "",
+      warning = "󰀪",
+      error = "󰅚",
+    },
   },
   log = {
-    enable = false,
+    enable = true,
     truncate = true,
     types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-      watcher = false,
+      all = true,
+      config = true,
+      copy_paste = true,
+      diagnostics = true,
+      git = true,
+      profile = true,
+      watcher = true,
     },
   },
 })
