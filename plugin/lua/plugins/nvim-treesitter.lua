a2b426ddc7b977c50c7859097788dfd47126aa5b
luCAOrx <lucaorx@protonmail.com>
2024-04-22 01:00:47 -0300

    chore: Add new lsps to be installed automatically and add some settings

--- plugin/lua/plugins/nvim-treesitter.lua
+++ plugin/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,32 @@
 require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = {
+    "bash",
+    "css",
+    "dockerfile",
+    "javascript",
+    "json",
+    "html",
+    "lua",
+    "vim",
+    "vimdoc",
+    "prisma",
+    "typescript",
+    "yaml"
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  -- sync_install = true,
   highlight = {
     enable = true,
+    use_languagetree = true,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  indent = { enable = true },
   rainbow = {
     enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
