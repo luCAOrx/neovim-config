29f7a1cf214c00d17ad29104528aaca3ff55419d
luCAOrx <lucaorx@protonmail.com>
2024-04-27 20:04:38 -0300

    chore: Remove property ensure_installed, add formatting by stylua

--- plugin/lua/configs/mason.lua
+++ plugin/lua/configs/mason.lua
@@ -1,34 +1,11 @@
 require("mason").setup({
-  ui = {
-    icons = {
-      package_pending = " ",
-      package_installed = "󰄳 ",
-      package_uninstalled = " ",
-    }
-  },
-
-    ensure_installed = {
-      "bashls",
-      "cspell",
-      "cssls",
-      "dockerls",
-      "emmet_language_server",
-      "eslint",
-      "html",
-      "jsonls",
-      "jsonlint",
-      "lua_ls",
-      "prettier",
-      "prismals",
-      "stylua",
-      "tailwindcss",
-      "tsserver",
-      "vimls",
-      "yamlls",
-      "yamlfmt"
-    },
-
-    automatic_installation = true,
+	ui = {
+		icons = {
+			package_pending = " ",
+			package_installed = "󰄳 ",
+			package_uninstalled = " ",
+		},
+	},
 })
 
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({})
