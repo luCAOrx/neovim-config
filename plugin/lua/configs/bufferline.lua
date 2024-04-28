9c353d46f9d8bc29b09126333ddc5f9db8e606b2
luCAOrx <lucaorx@protonmail.com>
2024-04-28 00:28:57 -0300

    style: Add formatting by stylua

--- plugin/lua/configs/bufferline.lua
+++ plugin/lua/configs/bufferline.lua
@@ -1,8 +1,8 @@
 vim.opt.termguicolors = true
-require("bufferline").setup {
-  options = {
-    separator_style = 'slant',
-    diagnostics = "nvim_lsp",
-    numbers = 'ordinal'
-  },
-}
+require("bufferline").setup({
+	options = {
+		separator_style = "slant",
+		diagnostics = "nvim_lsp",
+		numbers = "ordinal",
+	},
+})
