a1ed158cf0e8437bbb64aae46e9d593eee61cd5c
luCAOrx <lucaorx@protonmail.com>
2024-08-02 21:19:44 -0300

    chore: Add new fields and comments

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,10 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
+				modules = { "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
 				ensure_installed = {
 					"bash",
@@ -21,6 +23,16 @@ return {
 					"typescript",
 					"yaml",
 				},
+
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
+
+				-- List of parsers to ignore installing (or "all")
+				ignore_install = { "" },
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				-- sync_install = true,
